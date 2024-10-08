f96f55c60a242dfcc02888fd01bfd1f6702a0690
bipin0107 <bipinsingh007@gmail.com>
2024-10-08 11:00:30 +0530

    New file added.

---
+++ src/Java/StringOne.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class StringOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The number is divisible by 3" );
+
+	}
+
+}
